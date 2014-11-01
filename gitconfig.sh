0408546f7f1567ce69e4d5faa7c2d29711bba6ec
Adam Kosiorek <kosiorek.adam@gmail.com>
2014-11-01 10:10:00 +0100

    git config update

--- gitconfig.sh
+++ gitconfig.sh
@@ -5,3 +5,4 @@ git config --global user.email kosiorek.adam@gmail.com
 git config --global core.editor vim
 git config --global merge.tool vimdiff
 git config --global alias.report "log --pretty=format:'%h - %an %ad : %s' --author='Adam Kosiorek' --since=30.days -p"
+git config --global push.default simple
