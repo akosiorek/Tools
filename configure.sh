73f1ea570e1893e465194aeaf980272c86dbb7b8
Adam Kosiorek <kosiorek.adam@gmail.com>
2014-11-04 07:01:52 +0100

    changed bluetooth config to use sed instead of echo

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@
 gsettings set org.cinnamon.desktop.interface text-scaling-factor 1.1
 
 #	turn off bluetooh at login
-sh -c "echo 'rfkill block bluetooth' >> /etc/rc.local"
+sh -c "sed -i '$ i\rfkill block bluetooth' /etc/rc.local"
 
 ./gitconfig.sh
 ./autoboot.sh
